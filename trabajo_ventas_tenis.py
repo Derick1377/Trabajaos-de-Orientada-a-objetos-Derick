1de2fc5fe11458f0d4765e8834b727842fbc2237
Derick1377 <jandielramosderick@gmail.com>
2026-08-26 17:19:13 +0000

    Anade calculo de IVU al programa

--- trabajo_ventas_tenis.py
+++ trabajo_ventas_tenis.py
@@ -1,5 +1,7 @@
 print("DROP KICKS PR")
+
 print("-------------------------")
+
 print("1. Nike Air Force 1 - $120")
 print("2. Jordan 1 - $150")
 print("3. New Balance 550 - $110")
@@ -10,20 +12,25 @@ opcion = int(input("Seleccione una opción: "))
 if opcion == 1:
     tenis = "Nike Air Force 1"
     precio = 120
+
 elif opcion == 2:
     tenis = "Jordan 1"
     precio = 150
+
 elif opcion == 3:
     tenis = "New Balance 550"
     precio = 110
+
 elif opcion == 4:
     tenis = "Adidas Campus"
     precio = 100
+
 else:
     print("Opción no válida.")
     exit()
 
 print("\nTamaños disponibles:")
+
 print("6")
 print("7")
 print("8")
@@ -38,6 +45,7 @@ cantidad = int(input())
 total = precio * cantidad
 
 print("\n----- RESUMEN DE COMPRA -----")
+
 print("Tenis:", tenis)
 print("Size:", size)
 print("Cantidad:", cantidad)
